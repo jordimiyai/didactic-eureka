c9a9423ed07ff6b3b666bdcecd6fe16815ec65a9
jordimiyai <jgalvan89@gmail.com>
2022-04-10 20:47:26 -0300

    added feat to get balance, check if wallet is old and fixed address not found bug

--- api-wallets/src/wallets/wallets.model.ts
+++ api-wallets/src/wallets/wallets.model.ts
@@ -3,12 +3,12 @@ import * as mongoose from 'mongoose';
 export const WalletSchema = new mongoose.Schema({
   address: { type: String, required: true },
   isFavorite: { type: Boolean, default: false },
-  firstTransaction: {type: String, required: true}
+  firstTransaction: {type: Number, required: true}
 });
 
 export interface Wallet {
   id: string;
   address: string;
   isFavorite: boolean;
-  firstTransaction: string;
+  firstTransaction: number;
 }
